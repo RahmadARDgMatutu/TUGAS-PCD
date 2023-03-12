c3e55861f6fc008c0c4f1d4d6a062fdcaed90831
RahmadARDgMatutu <109814217+RahmadARDgMatutu@users.noreply.github.com>
2023-03-12 22:21:40 +0800

    Add files via upload

---
+++ Tugas 4/DFT/main.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Membaca gambar
+img = cv2.imread('mad.jpeg', 0)
+
+# Melakukan DFT pada gambar
+dft = cv2.dft(np.float32(img), flags=cv2.DFT_COMPLEX_OUTPUT)
+dft_shift = np.fft.fftshift(dft)
+
+# Mencari magnitude spectrum dari hasil DFT
+magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
+
+# Menampilkan gambar dan magnitude spectrum
+plt.subplot(121), plt.imshow(img, cmap='gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122), plt.imshow(magnitude_spectrum, cmap='gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
